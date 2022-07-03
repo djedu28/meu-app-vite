e6608fc1f3973da270fe6e896d0be913fcc742fc
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-07-03 02:43:51 -0300

    passo 2.1
    Add input dinamico

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import { useState } from 'react'
 import {Tweet} from './components/Tweet'
 import './App.css'
+
 function App() {
+  const [inputText, setText] = useState("Olá, Edu!")
   const [tweets, setTweets] = useState([
     "Olá Mundo!",
-    "02/07/2022 22:41"
+    "02/07/2022 22:41",
+    "Meu @ é @DjEdu28"
   ]);
   function createNewTweet(){
-    const txt:String = "TEste"
-    setTweets(x=>[...x,txt])
+    //const txt:String = "TEste"
+    setTweets(x=>[...x,inputText])
+    setText("")
   }
   return (
     <div className="App">
       {/* <h1>Olá Mundo!</h1>*/}
       {/* <p>02/07/2022 22:41</p> */}
       { tweets.map(txt =><Tweet text={txt}/>) }
-      <button onClick={createNewTweet}>Adicionar novo Tweet</button>
+      <input
+        type="text"
+        value={inputText}
+        onChange={event=>setText(event.target.value)}
+      />
+
+      <button
+        onClick={createNewTweet}
+      >
+        Adicionar novo Tweet
+      </button>
     </div>
   )
 }

3b11dc5ee2613875c6724e7b9468812774541135
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2022-07-03 01:59:52 -0300

    passo 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+//import { useState } from 'react'
+import {Tweet} from './components/Tweet'
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
-      <p>02/07/2022 22:41</p>
+      <Tweet text="Olá Mundo!"/> {/* <h1>Olá Mundo!</h1>*/}
+      <Tweet text="02/07/2022 22:41"/> {/* <p>02/07/2022 22:41</p> */}
     </div>
   )
 }
---
+++ src/components/Tweet.tsx
@@ -0,0 +1,9 @@
+type TweetProps = {
+  text: string,
+}
+
+export function Tweet(props:TweetProps) {
+  return (
+    <p> {props.text} </p>
+  )
+}
